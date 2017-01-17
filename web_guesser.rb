e0164395ecec6ac74043ccb297d1d3da85387447
James Anderson <james.p.anderson16@gmail.com>
2017-01-16 19:23:00 -0700

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,12 @@
 require "sinatra"
+require "sinatra/reloader"
+
+# get '/' do
+#   "Hello, World!"
+# end
+
+number = rand(100)
 
 get '/' do
-  "Hello, World!"
+"The SECRET NUMBER is #{number}"
 end
